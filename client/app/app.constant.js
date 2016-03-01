f72606bde0deb5c97b8e809117bb5b6f74a984e7
Dan Stelljes <dan@stellj.es>
2016-03-01 13:10:04 -0600

    I DID THIS

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('ulyssesApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','volunteer','organizer','admin']})
+
+;
+})(angular);
