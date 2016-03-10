59aa167e10aa0fb472f0cbd3db5310202d7a6b78
Dan Stelljes <stell124@morris.umn.edu>
2016-03-09 21:23:03 -0600

    (Dan and Elsa) Prettified schedule building, bumped Bootstrap version to 3.3.6, Papafied schedule input

--- client/app/schedule.input/schedule.input.controller.js
+++ client/app/schedule.input/schedule.input.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('ulyssesApp')
-  .controller('ScheduleInputCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ScheduleInputCtrl', function ($scope, papa) {
+    $scope.volunteerCSV = null;
+    $scope.volunteers = [];
+
+    $scope.process = function(data) {
+      if ($scope.volunteerCSV) {
+        papa.parse($scope.volunteerCSV, {
+          header: true,
+          complete: function(results) {
+            $scope.volunteers = results.data;
+            $scope.$apply();
+          }
+        });
+      }
+    };
   });
