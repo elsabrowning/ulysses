e92b009b3041b9128addae4676d510d6482ac173
brow3924 <brow3924@morris.umn.edu>
2016-03-22 13:52:22 -0500

    (everyone) pushed data to user. Papa had a kid

--- client/app/schedule.input/schedule.input.controller.js
+++ client/app/schedule.input/schedule.input.controller.js
@@ -1,19 +1,41 @@
 'use strict';
 
 angular.module('ulyssesApp')
-  .controller('ScheduleInputCtrl', function ($scope, papa) {
+  .controller('ScheduleInputCtrl', function ($scope, $stateParams, papa, Schedule) {
+    $scope.schedule = Schedule.get($stateParams);
     $scope.volunteerCSV = null;
-    $scope.volunteers = [];
 
     $scope.process = function(data) {
       if ($scope.volunteerCSV) {
         papa.parse($scope.volunteerCSV, {
           header: true,
-          complete: function(results) {
-            $scope.volunteers = results.data;
+          step: function(result) {
+            $scope.schedule.volunteers.push(birthVolunteer(result.data));
+          },
+          complete: function() {
             $scope.$apply();
           }
         });
       }
     };
+
+    var fullName = function(first, last){
+      return [first, last].join(" ");
+    };
+
+    var birthVolunteer = function(row){
+      return {
+        name: fullName(row["First name"], row["Last name"]),
+        email: row["E-mail"],
+        phone: row["phone"],
+        username: row["username"],
+        password: row["password"],
+        constraints: [],
+        comments: row["comment"],
+        shirt: row["T-shirt"],
+        positions: [],
+        preferences: []
+      };
+    };
+
   });
