a3e118a1e23224202eaa53453c7c82505f8f1533
Dan Stelljes <stell124@morris.umn.edu>
2016-04-05 13:37:18 -0500

    Made volunteer assignment work

--- client/app/schedule.edit/schedule.edit.controller.js
+++ client/app/schedule.edit/schedule.edit.controller.js
@@ -1,13 +1,41 @@
 'use strict';
 
 angular.module('ulyssesApp')
-  .controller('ScheduleEditCtrl', function ($scope, $stateParams, moment, Schedule) {
+  .controller('ScheduleEditCtrl', function ($scope, $state, $stateParams, moment, Schedule) {
     $scope.schedule = Schedule.get($stateParams);
 
-    $scope.range = function(slot) {
+    $scope.assign = function(volunteer, slot) {
+      var unassigned = $scope.schedule.unassigned;
+
+      // AH, PUSH IT
+      slot.assigned.push(unassigned.splice(unassigned.indexOf(volunteer), 1)[0]);
+    };
+
+    $scope.remainingPositions = function(slot) {
+      return Array(slot.positions - slot.assigned.length);
+    };
+
+    $scope.save = function() {
+      $scope.schedule.$save()
+        .then(function() {
+          $state.go('^.finalize');
+        }, function() {
+          console.log('An error happened / You write terrible software / Life is meaningless');
+        });
+    };
+
+    $scope.timeRange = function(slot) {
       var start = moment(slot.start);
       var end = moment(slot.end);
 
-      return start.format('h:mm') + ' to ' + end.format('h:mm');
+      return start.format('h:mma') + ' to ' + end.format('h:mma');
     };
+
+    $scope.unassign = function(volunteer, slot) {
+      var assigned = slot.assigned;
+
+      // PUSH IT REAL GOOD
+      $scope.schedule.unassigned.push(assigned.splice(assigned.indexOf(volunteer), 1)[0]);
+    };
+
   });
