c4807e2dcfec5c28d914d822f9593cca5bf233f9
Dan Stelljes <stell124@morris.umn.edu>
2016-04-07 13:43:46 -0500

    Processed some teams, yo

--- client/app/schedule.input/schedule.input.controller.js
+++ client/app/schedule.input/schedule.input.controller.js
@@ -3,6 +3,7 @@
 angular.module('ulyssesApp')
   .controller('ScheduleInputCtrl', function ($scope, $stateParams, papa, Schedule, $state) {
     $scope.schedule = Schedule.get($stateParams);
+    $scope.teamCSV = null;
     $scope.volunteerCSV = null;
 
     $scope.add = function() {
@@ -26,33 +27,38 @@ angular.module('ulyssesApp')
         });
       }
     };
-    // 
-    // $scope.processTeams = function(data) {
-    //   console.log("got to processTeams");
-    //
-    //   var numbers = [];
-    //
-    //   if ($scope.teamCSV) {
-    //     papa.parse($scope.teamCSV, {
-    //       header: true,
-    //       step: function(result) {
-    //         var row = result.data[0];
-    //
-    //         if (numbers.indexOf(row["Number"]) == -1) {
-    //           // do team birthing shit
-    //           numbers.push(row["Number"]);
-    //         }
-    //
-    //         // populate problems
-    //         addProblem()
-    //           $scope.schedule.teams.push(birthTeam(result.data[0]));
-    //       },
-    //       complete: function() {
-    //         $scope.$apply();
-    //       }
-    //     });
-    //   }
-    // };
+
+    $scope.processTeams = function(data) {
+      console.log("got to processTeams");
+
+      var conflicts = {};
+      var divisions = {
+        Primary: 0,
+        I: 1,
+        II: 2,
+        III: 3,
+        IV: 4
+      };
+
+      if ($scope.teamCSV) {
+        papa.parse($scope.teamCSV, {
+          header: true,
+          step: function(result) {
+            var row = result.data[0];
+            console.log(row['Longt time']);
+
+            conflicts['#' + row['Number'] + ' ' + row['Problem'] + '/' + divisions[row['Division']]] = {
+              start: moment(row['Longt Time'], 'h:mm A').subtract(15, 'minutes'),
+              end: moment(row['Longt Time'], 'h:mm A').add(1, 'hour')
+            };
+          },
+          complete: function() {
+            $scope.$apply();
+            console.log(conflicts);
+          }
+        });
+      }
+    };
 
     var fullName = function(first, last){
       return [first, last].join(" ");
@@ -71,24 +77,6 @@ angular.module('ulyssesApp')
         shirt: row["T-shirt"],
         positions: [],
         preferences: []
-      };
-    };
-
-    var birthTeam = function(row) {
-      if(row["Number"])
-      return {
-        number: row["Number"],
-        problems: [birthTeamInfo(row)]
-      };
-    };
-
-    var birthTeamInfo = function(row) { //sorry Kyle :(
-      console.log("This is the birth team");
-      return {
-        problem: row["Problem"],
-        division: row["Division"],
-        longterm: row["Longt Time"],
-        spontaneous: row["Spont Time"]
       };
     };
 
