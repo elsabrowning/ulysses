ef78397dd2e90e1246d547ec0dbe55ce08e390e4
brow3924 <brow3924@morris.umn.edu>
2016-04-13 18:49:14 -0500

    (Matt, Jake, Elsa) made time stuff for view, but no clue how to populate the times yet

--- client/app/schedule.view/schedule.view.controller.js
+++ client/app/schedule.view/schedule.view.controller.js
@@ -2,6 +2,25 @@
 
 angular.module('ulyssesApp')
   .controller('ScheduleViewCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.schedule = null;
     $scope.timeArray = [];
+    $scope.earlyTime = new Date('April 13, 2016, 07:00:00');
+
+    $scope.$parent.schedule.$promise.then(function(schedule) {
+      $scope.schedule = schedule;
+    });
+
+    $scope.populateTimeArray = function(date){
+      var earlyTime = moment($scope.earlyTime);
+      $scope.timeArray.push(earlyTime.format('h:mm a'));
+      for(var i = 1; i<25; i++){
+        $scope.timeArray.push(earlyTime.add(30, 'minutes').format('h:mm a'));
+      };
+    }
+    $scope.populateTimeArray();
+
+    $scope.numberOfJobs = function(){
+      
+    }
+
   });
