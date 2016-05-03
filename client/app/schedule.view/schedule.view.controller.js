152eb947ef0281a76cf2d62aa3faa340761e4e25
brow3924 <brow3924@morris.umn.edu>
2016-05-03 13:50:56 -0500

    (Jung, sometimes Dan, Elsa) flipped schedule view

--- client/app/schedule.view/schedule.view.controller.js
+++ client/app/schedule.view/schedule.view.controller.js
@@ -2,12 +2,12 @@
 
 angular.module('ulyssesApp')
   .controller('ScheduleViewCtrl', function ($scope) {
-    $scope.hour = 125;
+    $scope.hour = 134;
     $scope.schedule = null;
     $scope.timeArray = [];
     $scope.earlyTime = new Date('April 13, 2016, 07:00:00');
     $scope.lateTime = new Date('April 13, 2016, 17:00:00');
-    
+
     $scope.$parent.schedule.$promise.then(function(schedule) {
       $scope.schedule = schedule;
     });
@@ -15,15 +15,15 @@ angular.module('ulyssesApp')
     $scope.populateTimeArray = function(date){
       var earlyTime = moment($scope.earlyTime);
       $scope.timeArray.push(earlyTime.format('h:mm a'));
-      for(var i = 1; i<25; i++){
-        $scope.timeArray.push(earlyTime.add(30, 'minutes').format('h:mm a'));
+      for(var i = 1; i<13; i++){
+        $scope.timeArray.push(earlyTime.add(1, 'hours').format('h:mm a'));
       };
     }
     $scope.populateTimeArray();
 
     $scope.duration = function(time1, time2){
       return time2.getHours()-time1.getHours() + Math.abs(time2.getMinutes()-time1.getMinutes())/60;
-    }
+    }relative
 
     $scope.borderColorCode = function(slot){
       if(slot.assigned.length == 0){
@@ -51,9 +51,10 @@ angular.module('ulyssesApp')
       var offset = $scope.duration($scope.earlyTime, start);
       var shiftLength = $scope.duration(start, end);
 
+      console.log($scope.hour * offset + 100);
       return {
-        top: $scope.hour * offset + 1,
-        height: $scope.hour * shiftLength - 5,
+        left: $scope.hour * offset + 100,
+        width: $scope.hour * shiftLength - 6,
         backgroundColor: $scope.colorCode(slot),
         borderColor: $scope.borderColorCode(slot)
       };
